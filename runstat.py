3ab97fa5cb8c4fd67341e06d5619ae44f1964f94
Nath <nathanlloyd@protonmail.com>
2021-09-03 19:32:45 +0100

    Update runstat.py
    Added a nicer "saving" output on the terminal.  reworded some of the terminal I/O for more pleasant interaction.  Improved readability.  Changed all string formats to f-literal strings.  lastly improved extensibility by grouping instructions into discrete functions

--- runstat.py
+++ runstat.py
@@ -1,39 +1,54 @@
 #!/usr/bin/env python3
-import os
-import subprocess
+from os.path import isfile
+from subprocess import check_output
 
-def tx(x):
-  if b"/sa" in x:
-    b,e = x.split(b'/sa')
-    ret = b"%s/%04d%s" % (b,int(e[0:5])+5000, e[5:])
-    return ret
-  return x
+def tx(path):
+  if "/sa" in path:
+    begining, end = path.split("/sa")
+    numeral, name = int(end[:5]), end[5:]
+    return f"{begining}/{numeral+5000:05}{name}")
+  return path
 
+def git_commits():
+  # https://stackoverflow.com/questions/5669621/git-find-out-which-files-have-had-the-most-commits
+  filenames = set()
+  command = "git rev-list --objects --all | awk '$2' | sort -k2 | uniq -cf1 | sort -rn"
+  al_set = set()
+  for commit in check_output(command, shell=True).split('\n'):
+    commit_split = commit.split()
+    if len(commit_split) == 3:
+      count, _, filename = commit_split
+      count, filename = int(count), tx(filename)
+      if isfile(filename) and filename.startswith("masks/"):
+        if count > 1:
+          filenames.add(filename)
+        al_set.add(filename)
+  return al_set, filenames
 
-# https://stackoverflow.com/questions/5669621/git-find-out-which-files-have-had-the-most-commits
-out = subprocess.check_output("git rev-list --objects --all | awk '$2' | sort -k2 | uniq -cf1 | sort -rn", shell=True).strip().split(b"\n")
-fnn = []
-al_set = set()
-for j in out:
-  jj = j.strip().split(b" ")
-  if len(jj) != 3:
-    continue
-  cnt, _, fn = jj
-  fn = tx(fn)
-  cnt = int(cnt)
-  if os.path.isfile(fn) and fn.startswith(b"masks/"):
-    if cnt > 1:
-      fnn.append(fn)
-    al_set.add(fn)
-out = sorted(list(set(fnn)))
+def optional_output(al_set, filenames):
+  missing_count = len(al_set) - len(filenames)
+  question = (
+    f" Do you want to list {missing_count} mask"
+    f"{'s' * (missing_count > 0)} that are missing? [n]/y")
+  if input(question) == 'y':
+    print(al_set.difference(filenames))
 
-missing_count = len(al_set) - len(out)
-if missing_count < 20:
-  print(f"last {missing_count} mask(s) missing:")
-  print(al_set.difference(set(out)))
+def save(filenames):
+  # saving with a little style
+  print(" Saving", end='\r')
+  with open("files_trainable", "w") as trainables_file:
+    total = len(filenames)
+    for num, filename in enumerate(filenames):
+      trainables_file.write(filename + '\n')
+      print("\x1b[2K", " Saving {num/total:.0%}", end='\r')
+  print(" Saved", end="\r\x1b[2K")
 
-with open("files_trainable", "wb") as f:
-  f.write(b'\n'.join(out))
-print("number labelled %d/%d, percent done: %.2f%%" % (len(out), len(al_set), len(out)/len(al_set)*100.))
+if __name__ == "__main__":
+  al_set, filenames = git_commits()
+  
+  optional_output(al_set, filenames)
+  
+  save(filenames)
 
-
+  print(f" {len(filenames)} out of {len(al_set)} labelled,",
+        f" {len(filenames)/len(al_set):.2%} complete")
