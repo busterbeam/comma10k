d1253be86fc5341c4d1021d4dc0f686e7cd87ab5
Nath <nathanlloyd@protonmail.com>
2021-09-03 20:43:32 +0100

    Update convert_to_jpg.py
    provide a cooler output for file conversion also allowed input to change quality at runtime.  progress bar can adjust with window dimensions

--- tools/convert_to_jpg.py
+++ tools/convert_to_jpg.py
@@ -1,18 +1,41 @@
 #!/usr/bin/env python3
-import os
-from tqdm import tqdm
+from os import listdir, get_terminal_size
 from PIL import Image
 from multiprocessing import Pool
 
-def to_jpeg(fn):
-  fn = "imgs/"+fn
 
-  img = Image.open(fn)
-  img.save(fn.replace(".png", ".jpg"), quality=80)
+QUALITY = 80
+UP = "\x1b[2A\r"
+DOWN = "\x1b[2B\r"
+CLEAR = "\x1b[2K\r"
+
+def to_jpeg(filename):
+  global QUALITY
+  filename = f"imgs/{filename}"
+  
+  img = Image.open(filename)
+  filename = filename.replace(".png", ".jpg")
+  img.save(filename, quality=QUALITY)
+  return filename.lstrip("imgs/")
+
+def set_quality():
+  global QUALITY
+  try:
+    in_ = int(input(" Is a JPEG quality of 80 ok? [0-100]"))
+    QUALITY = in_ if 0 >= in_ <= 100 else QUALITY
+  except ValueError:
+    return False
 
 if __name__ == "__main__":
-  lst = sorted(filter(lambda x: x.endswith(".png"), os.listdir("imgs/")))
-
-  p = Pool(16)
-  for _ in tqdm(p.imap_unordered(to_jpeg, lst), total=len(lst)):
-    pass
+  images = sorted(filter(lambda x: x.endswith(".png"), listdir("imgs/")))
+  
+  set_quality()
+  
+  pool = Pool(16) # allow the pool choose the maximum amount of processes
+  total = len(images)
+  print(f" Converting PNG -> JPEG at {QUALITY}% quality")
+  for num, filename in enumerate(pool.imap_unordered(to_jpeg, images), start=1):
+    complete = num/total
+    width = list(get_terminal_size())[0] - 8
+    print(CLEAR, f"{'█'*int(complete*width):^{width}} {complete:4.0%}", end=DOWN)
+    print(CLEAR, f"Image: {filename}", end=UP)
