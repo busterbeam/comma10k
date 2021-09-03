81992670d01a24072062826c652307a8ad630a41
Nath <nathanlloyd@protonmail.com>
2021-09-03 18:42:35 +0100

    Update viewer.py
    Tried to improve readablility and extendability.  replacing `i` with `index` and `p` with `progress_bar` for example of readablility.  And extensibility has been improved by moving groups of instructions from the global space into discrete functions such as `init_window` `init_directory`, names could be improved.  Added fun emojis for the keyboard instructions as all terminal emulators should support emojis.

--- viewer.py
+++ viewer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-import os
-import sys
-import numpy as np
+from os import getenv, listdir, stat, system
+from os.path import isfile
+from sys import argv
+from numpy import asarray, uint8, array
 from tqdm import tqdm
 from PIL import Image
 
-NOSEGS = os.getenv("NOSEGS") is not None
+NOSEGS = getenv("NOSEGS") is not None
 
 def get_colormap(five=True):
   f32 = lambda x: (x % 256, x//256 % 256, x//(256*256) % 256)
@@ -13,96 +14,98 @@ def get_colormap(five=True):
     key = [2105408, 255, 0x608080, 6749952, 16711884]
   else:
     key = [0, 0xc4c4e2, 2105408, 255, 0x608080, 6749952, 16737792, 16711884]
-  return {i: f32(key[i]) for i in range(len(key))}
+  return {n: f32(k) for n, k in enumerate(key))}
 
 def gray_to_color(image, five=True):
   W,H = image.shape[0:2]
   colormap = get_colormap(five)
   c = image.ravel()
-  output = np.asarray([colormap[i] for i in c]) 
-  output = output.reshape((W, H, 3)).astype(np.uint8)
-  return output
+  output = asarray([colormap[i] for i in c])
+  return output.reshape((W, H, 3)).astype(uint8)
 
 def fix(im):
-  dat = np.array(im)
-  if im.mode == "P":
+  dat = array(im)
+  if im.mode == 'P':
     # palette image
-    pp = np.array(im.getpalette()).reshape((-1, 3)).astype(np.uint8)
-    sh = dat.shape
-    dat = pp[dat.flatten()].reshape(list(sh)+[3])
+    pp = array(im.getpalette()).reshape((-1, 3)).astype(uint8)
+    dat = pp[dat.flatten()].reshape(list(dat.shape)+[3])
 
   if dat.shape[2] != 3:
     # remove alpha
     dat = dat[:, :, 0:3]
   return dat
 
+def init_window():
+  from tools.window import Window
+  if getenv("IMGS2") is not None:
+    return Window(1928, 1208, halve=True)
+  else:
+    return Window(1164, 874)
+
+def init_directory(base_imgs):
+  lst = sorted(listdir(base_imgs))
+  if len(argv) > 1:
+    if isfile(argv[1]):
+      with open(argv[1]) as list_file:
+        lst = list_file.read().replace("masks/", '').strip().split('\n')
+    else:
+      # lst = list(filter(lambda x: x.startswith(("%04d" % int(argv[1]))), lst))
+      lst = lst[int(argv[1]):]
+  if getenv("ENTSORT") is not None:
+    lst = sorted(
+      lst, key=lambda x: int(stat(f"segs/{x}.npz").st_size), reverse=True)
+  return lst
+
+def display_instructions():
+  print("\n ⌨️  KEYBOARD COMMANDS:",
+         " ➡️  = step forward",
+         " ⬅️  = step back",
+         " ⬆️  = raise mask opacity",
+         " ⬇️  = lower mask opacity",
+         " m  = show/hide mask",
+         " s  = submit to scale API",
+         " q or ESC = quit\n", sep='\n')
 
 if __name__ == "__main__":
-  from tools.window import Window
-  import pygame
-  if os.getenv("IMGS2") is not None:
-    base_imgs = "imgs2/"
-    win = Window(1928, 1208, halve=True)
-  else:
-    base_imgs = "imgs/"
-    win = Window(1164, 874)
-  lst = sorted(os.listdir(base_imgs))
-  if len(sys.argv) > 1:
-    if os.path.isfile(sys.argv[1]):
-      lst = open(sys.argv[1]).read().replace("masks/", "").strip().split("\n")
-    else:
-      #lst = list(filter(lambda x: x.startswith(("%04d" % int(sys.argv[1]))), lst))
-      lst = lst[int(sys.argv[1]):]
-
-  if os.getenv("ENTSORT") is not None:
-    szz = []
-    for x in lst:
-      sz = os.stat("segs/"+x+".npz").st_size
-      szz.append((sz, x))
-    lst = [x[1] for x in sorted(szz, reverse=True)]
-
-  print("")
-  print("KEYBOARD COMMANDS:")
-  print("right arrow = step forward")
-  print("left arrow  = step back")
-  print("up arrow    = raise mask opacity")
-  print("down arrow  = lower mask opacity")
-  print("m           = show/hide mask")
-  print("q or escape = quit")
-  print("")
-  i = 0
+  from pygame import K_UP, K_DOWN, K_LEFT, K_RIGHT
+  win = init_window()
+  base_imgs = "imgs2/" if getenv("IMGS2") is not None else "imgs/"
+  filenames = init_directory(base_imgs)
+  display_instructions()
+  
+  next_keys = [K_RIGHT, ord(' '), ord('\n'), ord('\r')]
+  index = 0
   o = 2
-  m = True
-  p = tqdm(total=len(lst))
+  mask_state = True
+  progress_bar = tqdm(total=len(filenames))
   while True:
-    x = lst[i]
-    p.set_description(x)
-    p.n = (i % len(lst)) + 1
-    p.refresh()
+    filename = filenames[index]
+    progress_bar.set_description(filename)
+    progress_bar.n = (index % len(filenames)) + 1
+    progress_bar.refresh()
     while True:
-      ii = np.array(Image.open(base_imgs+x))
-      if not NOSEGS and os.path.isfile("masks/"+x) and m:
-        segi = fix(Image.open("masks/"+x))
+      ii = array(Image.open(f"{base_imgs}{filename}"))
+      if not NOSEGS and isfile(f"masks/{filename}") and mask_state:
+        segi = fix(Image.open(f"masks/{filename}"))
         # blend
-        ii = ii*((10-o)/10) + segi*(o/10)
+        ii = ii * ((10 - o) / 10) + segi * (o / 10)
       win.draw(ii)
-      kk = win.getkey()
-      if kk == ord("s"):
-        if not os.path.isfile("scale/response/%s" % x):
+      key_press = win.getkey()
+      if key_press == ord('s'):
+        if not isfile(f"scale/response/{filename}"):
           print("submitting to scaleapi")
-          os.system("scale/submit.sh "+x)
+          system(f"scale/submit.sh {filename}")
         else:
           print("ALREADY SUBMITTED!")
-      elif kk == ord('m'):
-        m = not m
-      elif kk == pygame.K_UP:
-        o = min(10, o+1)
-      elif kk == pygame.K_DOWN:
-        o = max(0, o-1)
-      elif kk in [pygame.K_RIGHT, ord(' '), ord('\n'), ord('\r')]:
-        i += 1
+      elif key_press == ord('m'):
+        mask_state = not mask_state
+      elif key_press == K_UP:
+        o = min(10, o + 1)
+      elif key_press == K_DOWN:
+        o = max(0, o - 1)
+      elif key_press in next_keys:
+        index += 1
         break
-      elif kk == pygame.K_LEFT:
-        i += -1
+      elif key_press == K_LEFT:
+        index -= 1
         break
-
